034c4cac84ed6ca24329e52f13ab486de641dc5d
Nakakakilig <sparaska64@gmail.com>
2025-02-20 14:29:15 +0200

    add naming conventions for alembic

--- profile-service/alembic/versions/2025_02_20_1428-d196c6abc4f1_create_user_table.py
+++ profile-service/alembic/versions/2025_02_20_1428-d196c6abc4f1_create_user_table.py
@@ -1,8 +1,8 @@
 """create user table
 
-Revision ID: 8f4f09294a27
+Revision ID: d196c6abc4f1
 Revises:
-Create Date: 2025-02-20 13:57:06.117349
+Create Date: 2025-02-20 14:28:03.137039
 
 """
 
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = "8f4f09294a27"
+revision: str = "d196c6abc4f1"
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -23,8 +23,8 @@ def upgrade() -> None:
         "users",
         sa.Column("id", sa.Integer(), nullable=False),
         sa.Column("username", sa.String(), nullable=False),
-        sa.PrimaryKeyConstraint("id"),
-        sa.UniqueConstraint("username"),
+        sa.PrimaryKeyConstraint("id", name=op.f("pk_users")),
+        sa.UniqueConstraint("username", name=op.f("uq_users_username")),
     )
 
 
