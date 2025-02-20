8b37e934758553c8b35621056983edf4dc78beb0
Nakakakilig <sparaska64@gmail.com>
2025-02-20 13:34:19 +0200

    remove useless row

--- profile-service/models/base.py
+++ profile-service/models/base.py
@@ -3,6 +3,4 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 class Base(DeclarativeBase):
 
-    __tablename__ = "base"
-
     id: Mapped[int] = mapped_column(primary_key=True)
