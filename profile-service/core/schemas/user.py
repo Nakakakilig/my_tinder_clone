28f641cf69324d6b7bd38066d4c1ebce7e6aae71
Nakakakilig <sparaska64@gmail.com>
2025-02-21 18:32:32 +0200

    add created/updated at in user model

--- profile-service/core/schemas/user.py
+++ profile-service/core/schemas/user.py
@@ -1,8 +1,11 @@
+from datetime import datetime
 from pydantic import BaseModel
 
 
 class UserBase(BaseModel):
     username: str
+    created_at: datetime | None
+    updated_at: datetime | None
 
 
 class UserCreate(UserBase):
