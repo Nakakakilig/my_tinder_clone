b337d46257102b2a13852fd6a9fbe2122b240c01
Nakakakilig <sparaska64@gmail.com>
2025-02-25 13:32:33 +0200

    make datetime optional for UserBase

--- profile-service/core/schemas/user.py
+++ profile-service/core/schemas/user.py
@@ -1,11 +1,12 @@
 from datetime import datetime
+from typing import Optional
 from pydantic import BaseModel
 
 
 class UserBase(BaseModel):
     username: str
-    created_at: datetime | None
-    updated_at: datetime | None
+    created_at: Optional[datetime] = None
+    updated_at: Optional[datetime] = None
 
 
 class UserCreate(UserBase):
