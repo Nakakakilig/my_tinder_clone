ef17ce0887d506884ef6cca0da6e056be7666c35
Nakakakilig <sparaska64@gmail.com>
2025-03-03 15:36:52 +0200

    generate fake preferences

---
+++ profile-service/app/utils/fake_data/create_preferences.py
@@ -0,0 +1,39 @@
+import os
+import random
+import sys
+
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))  # for core
+)
+
+sys.path.append(
+    os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "../../../../")  # for common
+    )
+)
+from core.db.db_helper import db_helper
+from core.models.preference import Preference
+
+from common.enums import Gender
+from common.preferences import PreferenceCreate
+
+
+async def create_multiple_preferences(N_preferences: int = 100):
+    preferences_creates = [
+        PreferenceCreate(
+            user_id=i,
+            gender=random.choice(list(Gender)),
+            age=random.randint(18, 60),
+            radius=random.randint(0, 20),
+        )
+        for i in range(1, N_preferences + 1)
+    ]
+
+    async for session in db_helper.session_getter():
+        preferences = [
+            Preference(**preferences_create.model_dump())
+            for preferences_create in preferences_creates
+        ]
+        session.add_all(preferences)
+        await session.commit()
+        print(f"Successfully created {len(preferences)} preferences")
--- profile-service/app/utils/fake_data/main.py
+++ profile-service/app/utils/fake_data/main.py
@@ -2,6 +2,7 @@ import asyncio
 
 from create_profiles import create_multiple_profiles
 from create_users import create_multiple_users
+from create_preferences import create_multiple_preferences
 
 N_USERS = 100
 
@@ -10,6 +11,7 @@ async def main():
     await asyncio.gather(
         create_multiple_users(N_USERS),
         create_multiple_profiles(N_profiles=N_USERS),
+        create_multiple_preferences(N_preferences=N_USERS),
     )
 
 
