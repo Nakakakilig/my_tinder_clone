59b319879b6a10b59684acbda33f1dfd454cefa0
Nakakakilig <sparaska64@gmail.com>
2025-03-05 19:43:29 +0200

    add exceptions for services in deck

--- deck-service/app/services/preference_client.py
+++ deck-service/app/services/preference_client.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 import httpx
 from core.config import settings
 
@@ -5,11 +6,19 @@ from core.schemas.preferences import PreferenceBase
 
 
 async def get_profile_preferences(profile_id: int) -> PreferenceBase:
-    async with httpx.AsyncClient() as client:
-        response = await client.get(
-            settings.preference_service.get_preference_url(profile_id)
-        )
-        response.raise_for_status()
-        profile = response.json()
-    profile = PreferenceBase(**profile)
-    return profile
+    try:
+        async with httpx.AsyncClient() as client:
+            response = await client.get(
+                settings.preference_service.get_preference_url(profile_id)
+            )
+            response.raise_for_status()
+            preference = response.json()
+        return PreferenceBase(**preference)
+
+    except httpx.HTTPStatusError as e:
+        if e.response.status_code == 404:
+            raise HTTPException(status_code=404, detail="Preference not found")
+        raise HTTPException(status_code=500, detail="Error while getting preferences")
+
+    except httpx.RequestError:
+        raise HTTPException(status_code=503, detail="Profile-service is unavailable")
--- deck-service/app/services/profile_client.py
+++ deck-service/app/services/profile_client.py
@@ -1,17 +1,16 @@
+from fastapi import HTTPException
 import httpx
 from core.config import settings
-
-from common.preferences import PreferenceBase
-from common.profile import ProfileRead, ProfileWithDistance
-
 from core.schemas.deck import MatchCard
+from core.schemas.preferences import PreferenceBase
+from core.schemas.profile import ProfileWithDistance
 
 
-async def get_candidate_profiles_for_deck(
+async def get_candidate_profiles(
     preferences: PreferenceBase,
     need_filter: bool,
     limit: int,
-) -> list[MatchCard]:
+) -> list | dict:
     params = {
         "gender": preferences.gender.value,
         "age": preferences.age,
@@ -19,26 +18,48 @@ async def get_candidate_profiles_for_deck(
         "limit": limit,
     }
     async with httpx.AsyncClient() as client:
-        response = await client.get(
-            settings.profile_service.get_matching_profiles_url(preferences.profile_id),
-            params=params if need_filter else None,
-        )
-        response.raise_for_status()
-        response_json: ProfileWithDistance = response.json()
-    profiles: list[ProfileRead] = [
-        ProfileRead(**candidate["profile"]) for candidate in response_json
-    ]
-    distances: list[float] = [candidate["distance_km"] for candidate in response_json]
+        try:
+            response = await client.get(
+                settings.profile_service.get_matching_profiles_url(
+                    preferences.profile_id
+                ),
+                params=params if need_filter else None,
+            )
+            response.raise_for_status()
+            return response.json()
 
-    res = []
-    for profile, distance in zip(profiles, distances):
-        card = MatchCard(
-            profile_id=profile.id,
-            first_name=profile.first_name,
-            last_name=profile.last_name,
-            gender=profile.gender.value,
-            age=profile.age,
-            distance_km=distance,
+        except httpx.HTTPStatusError as e:
+            if e.response.status_code == 404:
+                raise HTTPException(
+                    status_code=500,
+                    detail="Matching profiles not found. Change preferences!",
+                )
+
+            raise HTTPException(
+                status_code=500, detail="Error while getting preferences"
+            )
+
+        except httpx.RequestError:
+            raise HTTPException(
+                status_code=503, detail="Profile-service is unavailable"
+            )
+
+
+async def convert_to_model(
+    candidate_profiles_dict: list[dict],
+) -> list[MatchCard]:
+    profiles_with_distance: list[ProfileWithDistance] = [
+        ProfileWithDistance(**candidate) for candidate in candidate_profiles_dict
+    ]
+    candidates = [
+        MatchCard(
+            profile_id=prof_w_dist.profile.id,
+            first_name=prof_w_dist.profile.first_name,
+            last_name=prof_w_dist.profile.last_name,
+            gender=prof_w_dist.profile.gender.value,
+            age=prof_w_dist.profile.age,
+            distance_km=prof_w_dist.distance_km,
         )
-        res.append(card)
-    return res
+        for prof_w_dist in profiles_with_distance
+    ]
+    return candidates
