e9e0a1de7e493ca4b6e5e8249e56d112090480cb
Nakakakilig <sparaska64@gmail.com>
2025-03-05 19:45:59 +0200

    add db config in deck

--- deck-service/app/core/config.py
+++ deck-service/app/core/config.py
@@ -1,5 +1,8 @@
-from pydantic import BaseModel
-from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+from pydantic import BaseModel, PostgresDsn
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+load_dotenv()
 
 
 class ApiPrefix(BaseModel):
@@ -36,11 +39,29 @@ class PreferenceServicesConfig(BaseModel):
         return f"{self.preferences_base_url}/{preference_id}"
 
 
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    max_overflow: int = 10
+    pool_size: int = 50
+
+
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=(
+            "deck-service/.env.template",
+            "deck-service/.env",
+        ),
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="APP_CONFIG__",
+    )
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
     profile_service: ProfileServicesConfig = ProfileServicesConfig()
     preference_service: PreferenceServicesConfig = PreferenceServicesConfig()
+    db: DatabaseConfig
 
 
 settings = Settings()
