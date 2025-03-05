6a772af34f1260a89572983e69dd937bf799c7ee
Nakakakilig <sparaska64@gmail.com>
2025-03-05 20:06:16 +0200

    temporary comment table photo

--- deck-service/app/core/models/photo.py
+++ deck-service/app/core/models/photo.py
@@ -1,17 +1,17 @@
-from typing import TYPE_CHECKING
+# from typing import TYPE_CHECKING
 
-from core.db.base import Base
-from sqlalchemy import ForeignKey, Integer, String
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+# from core.db.base import Base
+# from sqlalchemy import ForeignKey, Integer, String
+# from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-if TYPE_CHECKING:
-    from .user import User
+# if TYPE_CHECKING:
+#     from .user import User
 
 
-class Photo(Base):
-    __tablename__ = "photos"
+# class Photo(Base):
+#     __tablename__ = "photos"
 
-    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"), index=True)
-    url: Mapped[str] = mapped_column(String(100), nullable=False)
+#     user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"), index=True)
+#     url: Mapped[str] = mapped_column(String(100), nullable=False)
 
-    user: Mapped["User"] = relationship(back_populates="photos")
+#     user: Mapped["User"] = relationship(back_populates="photos")
