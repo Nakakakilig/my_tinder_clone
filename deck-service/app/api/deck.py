661f6fce9c760f0b4e300fce141304e86b4ea904
Nakakakilig <sparaska64@gmail.com>
2025-03-05 12:29:57 +0200

    upgrade logic for get candidate ni deck

--- deck-service/app/api/deck.py
+++ deck-service/app/api/deck.py
@@ -1,34 +1,42 @@
 from api.deps import deck_dependency
-from core.schemas.deck import DeckRead
+from core.schemas.deck import MatchDeck
 from fastapi import APIRouter, HTTPException
 from services.deck_logic import generate_deck_logic
 from services.preference_client import get_profile_preferences
 from services.profile_client import get_candidate_profiles_for_deck
+from core.config import settings
 
-router = APIRouter(tags=["deck"])
+router = APIRouter(tags=["decks"])
 
 
-@router.post("/generate/{profile_id}", response_model=DeckRead)
-async def generate_deck(profile_id: int, cache: deck_dependency):
+@router.post("/{profile_id}/refresh", response_model=MatchDeck)
+async def generate_deck(
+    profile_id: int,
+    cache: deck_dependency,
+    need_filter: bool = False,
+    limit: int = settings.profile_service.limit_matched_profiles,
+):
     profile_preferences = await get_profile_preferences(profile_id)
-    print(f"{profile_preferences = }")
-    candidate_profiles = await get_candidate_profiles_for_deck(profile_preferences)
-    candidate_profiles
-    return
+    deck_cards = await get_candidate_profiles_for_deck(
+        profile_preferences,
+        need_filter=need_filter,
+        limit=limit,
+    )
+    deck = MatchDeck(profile_id=profile_id, candidates=deck_cards)
+    return deck
     if not profile_preferences:
         raise HTTPException(status_code=404, detail="Profile not found")
 
     deck_cards = generate_deck_logic(
         profile_id,
     )
-    deck = DeckRead(profile_id=profile_id, cards=deck_cards)
 
     await cache.set_deck(profile_id, deck.dict())
 
     return deck
 
 
-@router.get("/{profile_id}", response_model=DeckRead)
+@router.get("/{profile_id}", response_model=MatchDeck)
 async def get_deck(profile_id: int, cache: deck_dependency):
     deck_data = await cache.get_deck(profile_id)
     if deck_data is None:
@@ -36,5 +44,5 @@ async def get_deck(profile_id: int, cache: deck_dependency):
             status_code=404, detail="Deck not found. Please generate a deck first."
         )
 
-    deck = DeckRead(**deck_data)
+    deck = MatchDeck(**deck_data)
     return deck
--- deck-service/app/services/profile_client.py
+++ deck-service/app/services/profile_client.py
@@ -2,18 +2,43 @@ import httpx
 from core.config import settings
 
 from common.preferences import PreferenceBase
-from common.profile import ProfileRead
+from common.profile import ProfileRead, ProfileWithDistance
+
+from core.schemas.deck import MatchCard
 
 
 async def get_candidate_profiles_for_deck(
     preferences: PreferenceBase,
-) -> list[ProfileRead]:
-    # TODO: add filter (better filter in Select, then in backend)
-
+    need_filter: bool,
+    limit: int,
+) -> list[MatchCard]:
+    params = {
+        "gender": preferences.gender.value,
+        "age": preferences.age,
+        "radius": preferences.radius,
+        "limit": limit,
+    }
     async with httpx.AsyncClient() as client:
-        # TODO: change get_all_profiles_url to 'get candidates that match with preferences'
-        response = await client.get(settings.profile_service.get)
+        response = await client.get(
+            settings.profile_service.get_matching_profiles_url(preferences.profile_id),
+            params=params if need_filter else None,
+        )
         response.raise_for_status()
-        profiles = response.json()
-    profiles = [ProfileRead(**profile) for profile in profiles]
-    return profiles
+        response_json: ProfileWithDistance = response.json()
+    profiles: list[ProfileRead] = [
+        ProfileRead(**candidate["profile"]) for candidate in response_json
+    ]
+    distances: list[float] = [candidate["distance_km"] for candidate in response_json]
+
+    res = []
+    for profile, distance in zip(profiles, distances):
+        card = MatchCard(
+            profile_id=profile.id,
+            first_name=profile.first_name,
+            last_name=profile.last_name,
+            gender=profile.gender.value,
+            age=profile.age,
+            distance_km=distance,
+        )
+        res.append(card)
+    return res
