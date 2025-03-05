7e80af94dc05e6d6ce973d8e57c03b2aeb97fa01
Nakakakilig <sparaska64@gmail.com>
2025-03-05 12:18:48 +0200

    change value in fake radius preferences

--- profile-service/app/utils/fake_data/create_preferences.py
+++ profile-service/app/utils/fake_data/create_preferences.py
@@ -25,7 +25,7 @@ async def create_multiple_preferences(N_preferences: int = 100):
             profile_id=i,
             gender=random.choice(list(Gender)),
             age=random.randint(18, 60),
-            radius=random.randint(0, 20),
+            radius=random.randint(0, 400),
         )
         for i in range(1, N_preferences + 1)
     ]
