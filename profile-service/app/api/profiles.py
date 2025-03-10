15cb2704dabca2e2859d5c5eb65757684ad4b450
Nakakakilig <sparaska64@gmail.com>
2025-03-10 15:29:55 +0200

    add services logic in profile create

--- profile-service/app/api/profiles.py
+++ profile-service/app/api/profiles.py
@@ -4,6 +4,11 @@ from crud import profiles as profiles_crud
 from fastapi import APIRouter, HTTPException
 
 from api.deps import db_dependency
+from services.profile import (
+    create_profile_service,
+    get_profile_service,
+    get_profiles_service,
+)
 
 router = APIRouter(tags=["profiles"])
 
@@ -12,8 +17,7 @@ router = APIRouter(tags=["profiles"])
 async def get_profiles(
     session: db_dependency,
 ):
-    profiles = await profiles_crud.get_all_profiles(session=session)
-    return profiles
+    return await get_profiles_service(session)
 
 
 @router.post("/", response_model=ProfileRead)
@@ -22,11 +26,7 @@ async def create_profile(
     profile_create: ProfileCreate,
     # TODO in future:  user_id: UUID = Depends(get_user_id_from_JWT_token)
 ) -> ProfileRead:
-    profile = await profiles_crud.create_profile(
-        session=session,
-        profile_create=profile_create,
-    )
-    return profile
+    return await create_profile_service(session, profile_create)
 
 
 @router.get("/{profile_id}", response_model=ProfileRead)
@@ -34,13 +34,7 @@ async def get_profile(
     session: db_dependency,
     profile_id: int,
 ) -> ProfileRead:
-    profile = await profiles_crud.get_profile(
-        session=session,
-        profile_id=profile_id,
-    )
-    if not profile:
-        raise HTTPException(status_code=404, detail="Profile not found")
-    return profile
+    return await get_profile_service
 
 
 @router.get("/{profile_id}/matches", response_model=list[ProfileWithDistance])
---
+++ profile-service/app/services/profile.py
@@ -0,0 +1,53 @@
+from core.models.profile import Profile
+from core.schemas.profile import ProfileCreate
+from crud import profiles as crud_profile
+from fastapi import HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+async def get_profiles_service(db: AsyncSession) -> list[Profile]:
+    try:
+        profiles = await crud_profile.get_all_profiles(db)
+    except ConnectionError:
+        raise HTTPException(
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+            detail="Database connection refused. Please check if the DB is running.",
+        )
+
+    if not profiles:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Profiles not found",
+        )
+    return profiles
+
+
+async def create_profile_service(
+    db: AsyncSession,
+    profile_create: ProfileCreate,
+) -> Profile:
+    existing_profile = await crud_profile.get_profile(db, profile_create.user_id)
+
+    if existing_profile:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Profile already exists.",
+        )
+
+    profile = await crud_profile.create_profile(db, profile_create)
+    if not profile:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Profile creation failed.",
+        )
+    return profile
+
+
+async def get_profile_service(db: AsyncSession, profile_id: int) -> Profile:
+    profile = await crud_profile.get_profile(db, profile_id)
+    if not profile:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Profile not found",
+        )
+    return profile
