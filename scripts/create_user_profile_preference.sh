3991d33843fc7983b4df578954cca98b31889d2e
Nakakakilig <sparaska64@gmail.com>
2025-03-11 16:14:55 +0200

    add .sh for create user+profile+preference

---
+++ scripts/create_user_profile_preference.sh
@@ -0,0 +1,39 @@
+random_username=$(date +%s)
+
+user_id=$(
+  curl -X 'POST' \
+  'http://0.0.0.0:8000/api/users/create' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "username": "'$(date +%s)'"
+}'| jq -r '.id'
+)
+
+
+profile_id=$(
+  curl -X 'POST' \
+  'http://0.0.0.0:8000/api/profiles/' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d $'{
+  "user_id": '"$user_id"',
+  "first_name": "string",
+  "last_name": "string",
+  "gender": "male",
+  "age": 0,
+  "geo_latitude": 0,
+  "geo_longitude": 0
+}'| jq -r '.id'
+)
+
+curl -X 'POST' \
+  'http://0.0.0.0:8000/api/preferences/' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "profile_id": '"$profile_id"',
+  "gender": "male",
+  "age": 0,
+  "radius": 0
+}'
