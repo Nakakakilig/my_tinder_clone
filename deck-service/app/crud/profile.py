450c0665afe60bb903db1c838331c3aa6fe29f62
Nakakakilig <sparaska64@gmail.com>
2025-03-18 13:33:50 +0200

    in deck-service rewrite logic of find matched profiles

--- deck-service/app/crud/profile.py
+++ deck-service/app/crud/profile.py
@@ -1,10 +1,12 @@
 from typing import Sequence
 
-from sqlalchemy import select
+from sqlalchemy import and_, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.models.profile import Profile
 from app.core.schemas.profile import ProfileCreate
+from app.core.schemas.preferences import PreferenceBase
+from app.utils.calc_distance import calc_distance_in_query
 
 
 async def create_profile(
@@ -28,3 +30,43 @@ async def get_profile_by_outer_id(session: AsyncSession, outer_id: int) -> Profi
     stmt = select(Profile).where(Profile.outer_id == outer_id)
     result = await session.scalars(stmt)
     return result.one_or_none()
+
+
+async def get_matching_profiles(
+    session: AsyncSession,
+    profile_id: int,
+    preferences: PreferenceBase,
+    limit: int,
+):
+    current_profile = await session.get(Profile, profile_id)
+    if not current_profile:
+        return None
+
+    distance_expr = calc_distance_in_query(current_profile, Profile)
+
+    filters = [
+        Profile.id != current_profile.id,
+        Profile.gender == preferences.gender,
+        Profile.age >= preferences.age - 5,
+        Profile.age <= preferences.age + 5,
+    ]
+
+    query = (
+        select(Profile, distance_expr)
+        .filter(and_(*filters))
+        .group_by(Profile.id)
+        .order_by(distance_expr)
+        .limit(limit)
+    )
+
+    query = query.having(distance_expr <= preferences.radius)
+
+    result = await session.execute(query)
+    profiles: list[tuple[Profile, float]] = result.all()
+
+    matching_profiles = [
+        {"profile": profile.__dict__, "distance_km": float(distance)}
+        for profile, distance in profiles
+    ]
+
+    return matching_profiles
---
+++ deck-service/app/utils/calc_distance.py
@@ -0,0 +1,28 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy import Numeric, cast, func
+
+if TYPE_CHECKING:
+    from app.core.models.profile import Profile
+
+
+def calc_distance_in_query(current_profile: "Profile", Profile: "Profile"):
+    distance_expr = func.round(
+        cast(
+            6371  # radius of the earth in km
+            * func.acos(
+                func.cos(func.radians(current_profile.geo_latitude))
+                * func.cos(func.radians(Profile.geo_latitude))
+                * func.cos(
+                    func.radians(Profile.geo_longitude)
+                    - func.radians(current_profile.geo_longitude)
+                )
+                + func.sin(func.radians(current_profile.geo_latitude))
+                * func.sin(func.radians(Profile.geo_latitude))
+            ),
+            Numeric,
+        ),
+        2,  # round 2 decimal places
+    ).label("distance_km")
+
+    return distance_expr
