6dadeb300b4651bdbaad59c17f40ac585f8bd0ca
Nakakakilig <sparaska64@gmail.com>
2025-03-18 13:35:06 +0200

    remove useless funcion

--- deck-service/app/services/profile_client.py
+++ deck-service/app/services/profile_client.py
@@ -1,48 +1,5 @@
-import httpx
-from app.core.config import settings
 from app.core.schemas.deck import MatchCard
-from app.core.schemas.preferences import PreferenceBase
 from app.core.schemas.profile import ProfileWithDistance
-from fastapi import HTTPException
-
-
-async def get_candidate_profiles(
-    preferences: PreferenceBase,
-    need_filter: bool,
-    limit: int,
-) -> list | dict:
-    params = {
-        "gender": preferences.gender.value,
-        "age": preferences.age,
-        "radius": preferences.radius,
-        "limit": limit,
-    }
-    async with httpx.AsyncClient() as client:
-        try:
-            response = await client.get(
-                settings.profile_service.get_matching_profiles_url(
-                    preferences.profile_id
-                ),
-                params=params if need_filter else None,
-            )
-            response.raise_for_status()
-            return response.json()
-
-        except httpx.HTTPStatusError as e:
-            if e.response.status_code == 404:
-                raise HTTPException(
-                    status_code=500,
-                    detail="Matching profiles not found. Change preferences!",
-                )
-
-            raise HTTPException(
-                status_code=500, detail="Error while getting preferences"
-            )
-
-        except httpx.RequestError:
-            raise HTTPException(
-                status_code=503, detail="Profile-service is unavailable"
-            )
 
 
 async def convert_to_model(
