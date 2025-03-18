2833875443003a98a82a78f7da592dab0b15fabe
Nakakakilig <sparaska64@gmail.com>
2025-03-18 13:32:38 +0200

    in deck-service remove useless configs

--- deck-service/app/core/config.py
+++ deck-service/app/core/config.py
@@ -15,28 +15,8 @@ class RunConfig(BaseModel):
     port: int = 8001
 
 
-class ProfileServicesConfig(BaseModel):
-    profiles_base_url: str = "http://0.0.0.0:8000/api/profiles"
-    limit_matched_profiles: int = 10
-
-    def get_all_profiles_url(self) -> str:
-        return f"{self.profiles_base_url}/get-all"
-
-    def get_profile_url(self, profile_id: int) -> str:
-        return f"{self.profiles_base_url}/{profile_id}"
-
-    def get_matching_profiles_url(self, profile_id: int) -> str:
-        return f"{self.profiles_base_url}/{profile_id}/matches"
-
-
-class PreferenceServicesConfig(BaseModel):
-    preferences_base_url: str = "http://0.0.0.0:8000/api/preferences"
-
-    def get_all_preferences_url(self) -> str:
-        return f"{self.preferences_base_url}/get-all"
-
-    def get_preference_url(self, preference_id: int) -> str:
-        return f"{self.preferences_base_url}/{preference_id}"
+class DeckConfig(BaseModel):
+    limit_matched_profiles: int = 15
 
 
 class DatabaseConfig(BaseModel):
@@ -65,8 +45,7 @@ class Settings(BaseSettings):
     )
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
-    profile_service: ProfileServicesConfig = ProfileServicesConfig()
-    preference_service: PreferenceServicesConfig = PreferenceServicesConfig()
+    deck: DeckConfig = DeckConfig()
     db: DatabaseConfig
     kafka: KafkaConfig = KafkaConfig()
 
