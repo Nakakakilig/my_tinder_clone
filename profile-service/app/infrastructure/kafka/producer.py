d478c4c814acabf3f4fb55597501f0a375e1d675
Nakakakilig <sparaska64@gmail.com>
2025-03-24 20:19:22 +0200

    Refactor KafkaProducer class to implement lazy initialization for the AIOKafkaProducer instance, improving resource management and ensuring the producer is only created when needed.

--- profile-service/app/infrastructure/kafka/producer.py
+++ profile-service/app/infrastructure/kafka/producer.py
@@ -3,26 +3,35 @@ from enum import Enum
 
 from aiokafka import AIOKafkaProducer
 from pydantic import BaseModel
-# from config.settings import settings
 
 
 class KafkaProducer:
     def __init__(self, bootstrap_servers: str):
-        self.producer = AIOKafkaProducer(
-            bootstrap_servers=bootstrap_servers,
-            value_serializer=lambda v: json.dumps(
-                v, default=lambda x: x.value if isinstance(x, Enum) else str(x)
-            ).encode("utf-8"),
-        )
+        self.bootstrap_servers = bootstrap_servers
+        self._producer = None
+
+    @property
+    def producer(self) -> AIOKafkaProducer:
+        if self._producer is None:
+            self._producer = AIOKafkaProducer(
+                bootstrap_servers=self.bootstrap_servers,
+                value_serializer=lambda v: json.dumps(
+                    v, default=lambda x: x.value if isinstance(x, Enum) else str(x)
+                ).encode("utf-8"),
+            )
+        return self._producer
 
     async def start(self):
-        if not self.producer:
-            await self.producer.start()
+        if not self._producer:
+            self._producer = self.producer
+            await self._producer.start()
 
     async def stop(self):
-        if self.producer:
-            await self.producer.stop()
+        if self._producer:
+            await self._producer.stop()
+            self._producer = None
 
     async def send_event(self, topic: str, event: BaseModel):
-        message = json.dumps(event.model_dump(), default=str).encode("utf-8")
-        await self.producer.send(topic, message)
+        if not self._producer:
+            await self.start()
+        await self.producer.send(topic, event)
