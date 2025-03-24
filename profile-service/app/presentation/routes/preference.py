5ce824063fcdd422380298ad6b93f096800b4175
Nakakakilig <sparaska64@gmail.com>
2025-03-24 13:00:38 +0200

    Refactor preference schemas to use consistent naming conventions; update related routes and fake data generation accordingly

--- profile-service/app/presentation/routes/preference.py
+++ profile-service/app/presentation/routes/preference.py
@@ -1,40 +1,40 @@
 from application.services.preference import PreferenceService
-from application.schemas.preference import PreferenceCreate, PreferenceRead
+from application.schemas.preference import PreferenceCreateSchema, PreferenceReadSchema
 from fastapi import APIRouter, Depends
 from presentation.dependencies.preference import get_preference_service
 
 router = APIRouter(tags=["preferences"])
 
 
-@router.get("/", response_model=list[PreferenceRead])
+@router.get("/", response_model=list[PreferenceReadSchema])
 async def get_preferences(
     preference_service: PreferenceService = Depends(get_preference_service),
 ):
     return await preference_service.get_preferences()
 
 
-@router.post("/", response_model=PreferenceRead)
+@router.post("/", response_model=PreferenceReadSchema)
 async def create_preference(
-    preference_create: PreferenceCreate,
+    preference_create: PreferenceCreateSchema,
     preference_service: PreferenceService = Depends(get_preference_service),
     # TODO in future:  user_id: UUID = Depends(get_user_id_from_JWT_token)
-) -> PreferenceRead:
+) -> PreferenceReadSchema:
     return await preference_service.create_preference(preference_create)
 
 
-@router.get("/{preference_id}", response_model=PreferenceRead)
+@router.get("/{preference_id}", response_model=PreferenceReadSchema)
 async def get_preference_by_id(
     preference_id: int,
     preference_service: PreferenceService = Depends(get_preference_service),
-) -> PreferenceRead:
+) -> PreferenceReadSchema:
     return await preference_service.get_preference_by_id(preference_id)
 
 
-@router.get("/profile/{profile_id}", response_model=PreferenceRead)
+@router.get("/profile/{profile_id}", response_model=PreferenceReadSchema)
 async def get_preference_by_profile_id(
     profile_id: int,
     preference_service: PreferenceService = Depends(get_preference_service),
-) -> PreferenceRead:
+) -> PreferenceReadSchema:
     return await preference_service.get_preference_by_profile_id(profile_id)
 
 
