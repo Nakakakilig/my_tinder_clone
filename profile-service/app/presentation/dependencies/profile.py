79944c77e955a1267a48b8aa696004edfcbd715a
Nakakakilig <sparaska64@gmail.com>
2025-03-24 20:21:04 +0200

    Integrate Kafka producer dependency into preference and profile service

--- profile-service/app/presentation/dependencies/profile.py
+++ profile-service/app/presentation/dependencies/profile.py
@@ -3,6 +3,8 @@ from typing import AsyncGenerator
 from application.services.profile import ProfileService
 from fastapi import Depends
 from infrastructure.db.db_helper import db_helper
+from infrastructure.kafka.init import get_kafka_producer
+from infrastructure.kafka.producer import KafkaProducer
 from infrastructure.repositories_impl.profile import ProfileRepositoryImpl
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -14,9 +16,7 @@ async def get_db_session() -> AsyncGenerator[AsyncSession, None]:
 
 def get_profile_service(
     db_session: AsyncSession = Depends(get_db_session),
+    kafka_producer: KafkaProducer = Depends(get_kafka_producer),
 ) -> ProfileService:
-    from main import get_kafka_producer
-
-    kafka_producer = get_kafka_producer()
     profile_repository = ProfileRepositoryImpl(db_session)
     return ProfileService(profile_repository, kafka_producer)
