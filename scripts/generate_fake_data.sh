274239c243695440586bbc4385ee751471b12c11
Nakakakilig <sparaska64@gmail.com>
2025-03-24 20:13:44 +0200

    update directiry for fake data

--- scripts/generate_fake_data.sh
+++ scripts/generate_fake_data.sh
@@ -12,9 +12,7 @@ echo -e "\nActivate venv ..."
 source .venv/bin/activate
 
 echo -e "\nRun profile-service/app/utils/fake_data/main.py ..."
-cd profile-service
-# python3 -m app.utils.fake_data.main
-cd app
+cd profile-service/app
 python3 -m utils.fake_data.main
 echo -e "\nDeactivate venv ..."
 deactivate
