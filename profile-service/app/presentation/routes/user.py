bcb8c6f809102fc35ec5c9dffcb2125586a29569
Nakakakilig <sparaska64@gmail.com>
2025-03-24 13:00:26 +0200

    Refactor user schemas to use consistent naming conventions; update routes and fake data generation accordingly

--- profile-service/app/presentation/routes/user.py
+++ profile-service/app/presentation/routes/user.py
@@ -1,4 +1,4 @@
-from application.schemas.user import UserCreate, UserRead
+from application.schemas.user import UserCreateSchema, UserReadSchema
 from application.services.user import UserService
 from fastapi import APIRouter, Depends
 from presentation.dependencies.user import get_user_service
@@ -6,26 +6,26 @@ from presentation.dependencies.user import get_user_service
 router = APIRouter(tags=["users"])
 
 
-@router.post("/", response_model=UserRead)
+@router.post("/", response_model=UserReadSchema)
 async def create_user(
-    user_create: UserCreate,
+    user_create: UserCreateSchema,
     user_service: UserService = Depends(get_user_service),
-) -> UserRead:
+) -> UserReadSchema:
     # TODO: somehow create profile after user creation
     user = await user_service.create_user(user_create)
     return user
 
 
-@router.get("/", response_model=list[UserRead])
+@router.get("/", response_model=list[UserReadSchema])
 async def get_users(
     user_service: UserService = Depends(get_user_service),
-) -> list[UserRead]:
+) -> list[UserReadSchema]:
     return await user_service.get_users()
 
 
-@router.get("/{user_id}", response_model=UserRead)
+@router.get("/{user_id}", response_model=UserReadSchema)
 async def get_user(
     user_id: int,
     user_service: UserService = Depends(get_user_service),
-) -> UserRead:
+) -> UserReadSchema:
     return await user_service.get_user_by_id(user_id)
