9c53a3ae903dcaac8033f6ac4755c6e1320e4226
Nakakakilig <sparaska64@gmail.com>
2025-03-25 17:51:58 +0200

    mv services to application/services

---
+++ deck-service/app/application/services/profile.py
@@ -0,0 +1,16 @@
+from domain.models.profile import Profile
+from domain.repositories.profile import IProfileRepository
+
+
+class ProfileService:
+    def __init__(
+        self,
+        profile_repository: IProfileRepository,
+    ):
+        self.profile_repository = profile_repository
+
+    async def get_profile_by_id(self, profile_id: int) -> Profile:
+        return await self.profile_repository.get_profile_by_id(profile_id)
+
+    async def get_profiles(self) -> list[Profile]:
+        return await self.profile_repository.get_profiles()
