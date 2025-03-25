d1ad6cb8cc02f39859299d018b8d71ad02c137b4
Nakakakilig <sparaska64@gmail.com>
2025-03-25 17:54:53 +0200

    add reposotories_impl to infrastrucre

---
+++ deck-service/app/infrastructure/repositories_impl/profile.py
@@ -0,0 +1,27 @@
+from domain.models.profile import Profile
+from domain.repositories.profile import IProfileRepository
+from infrastructure.db.db_models import ProfileORM
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+class ProfileRepositoryImpl(IProfileRepository):
+    def __init__(self, db_session: AsyncSession):
+        self.db_session = db_session
+
+    async def get_profile_by_id(self, profile_id: int) -> ProfileORM | None:
+        stmt = select(ProfileORM).where(ProfileORM.id == profile_id)
+        result = await self.db_session.execute(stmt)
+        return result.scalar_one_or_none()
+
+    async def create_profile(self, profile: Profile) -> ProfileORM:
+        profile_orm = ProfileORM(**profile.model_dump())
+        self.db_session.add(profile_orm)
+        await self.db_session.commit()
+        await self.db_session.refresh(profile_orm)
+        return profile_orm
+
+    async def get_profiles(self) -> list[ProfileORM]:
+        stmt = select(ProfileORM).order_by(ProfileORM.id)
+        result = await self.db_session.execute(stmt)
+        return result.scalars().all()
