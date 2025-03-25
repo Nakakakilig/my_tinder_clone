89aa979be785f018ed89c58b47f4b647e3f9e4af
Nakakakilig <sparaska64@gmail.com>
2025-03-25 17:55:07 +0200

    refactor main.py to align with new project structure by updating import paths and router inclusion

--- deck-service/app/main.py
+++ deck-service/app/main.py
@@ -3,9 +3,9 @@ from contextlib import asynccontextmanager
 import uvicorn
 from fastapi import FastAPI
 
-from app.api.main import router as deck_router
-from app.core.config import settings
-from app.kafka.consumer import start_consumer_loop
+from config.settings import settings
+from infrastructure.kafka.consumer import start_consumer_loop
+from presentation.routes.main import router
 
 
 @asynccontextmanager
@@ -15,13 +15,13 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
-app.include_router(deck_router)
+app.include_router(router)
 
 # TODO: add redis connect/shutdown to lifespan
 
 if __name__ == "__main__":
     uvicorn.run(
-        "app.main:app",
+        "main:app",
         host=settings.run.host,
         port=settings.run.port,
         reload=True,
