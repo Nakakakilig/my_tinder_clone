8ab905e1327fef9b511446353ad907dfefc38ba8
Nakakakilig <sparaska64@gmail.com>
2025-03-25 15:00:45 +0200

    Refactor dependency injection in preference, profile, and user services to utilize centralized db_session dependency, removing redundant session getter implementations.

--- profile-service/app/presentation/dependencies/user.py
+++ profile-service/app/presentation/dependencies/user.py
@@ -1,17 +1,12 @@
-from typing import AsyncGenerator
-
 from application.services.user import UserService
 from fastapi import Depends
-from infrastructure.db.db_helper import db_helper
 from infrastructure.repositories_impl.user import UserRepositoryImpl
+from presentation.dependencies.db_session import get_db_session
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
-async def get_db_session() -> AsyncGenerator[AsyncSession, None]:
-    async for session in db_helper.session_getter():
-        yield session
-
-
-def get_user_service(db_session: AsyncSession = Depends(get_db_session)) -> UserService:
+def get_user_service(
+    db_session: AsyncSession = Depends(get_db_session),
+) -> UserService:
     user_repository = UserRepositoryImpl(db_session)
     return UserService(user_repository)
