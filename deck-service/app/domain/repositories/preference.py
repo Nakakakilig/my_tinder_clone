86024c3b60f9703e2d7bf183a12301eec023e722
Nakakakilig <sparaska64@gmail.com>
2025-03-25 17:54:10 +0200

    add domain reposotories

---
+++ deck-service/app/domain/repositories/preference.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+from domain.models.preference import Preference
+
+
+class IPreferenceRepository(ABC):
+    @abstractmethod
+    async def get_preference_by_id(self, preference_id: int) -> Preference | None:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def get_preference_by_profile_id(self, profile_id: int) -> Preference | None:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def get_preferences(self) -> list[Preference]:
+        raise NotImplementedError
