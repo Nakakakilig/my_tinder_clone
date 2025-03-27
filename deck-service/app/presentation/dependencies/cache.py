bef5d543c54001e94952a0b8ca12cf1143df7bc8
Nakakakilig <sparaska64@gmail.com>
2025-03-27 18:06:15 +0200

    Add Redis client dependency and cache retrieval function for improved caching support

---
+++ deck-service/app/presentation/dependencies/cache.py
@@ -0,0 +1,20 @@
+from typing import AsyncGenerator
+from fastapi import Depends
+from redis import asyncio as aioredis
+
+from config.settings import settings
+from domain.repositories.cache import ICache
+from infrastructure.cache.factory import CacheFactory
+
+
+async def get_redis_client() -> AsyncGenerator[aioredis.Redis, None]:
+    redis = await aioredis.from_url(settings.redis.url, decode_responses=True)
+    try:
+        yield redis
+    finally:
+        await redis.close()
+
+
+def get_cache(redis_client: aioredis.Redis = Depends(get_redis_client)) -> ICache:
+    # return CacheFactory.create_memory_cache()
+    return CacheFactory.create_redis_cache(redis_client)
