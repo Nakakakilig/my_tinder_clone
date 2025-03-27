0d0a36c1da72343e6b284a57c9fb42dad6e0cd71
Nakakakilig <sparaska64@gmail.com>
2025-03-27 18:07:59 +0200

    Add get_deck_service function to provide DeckService with dependencies for database session and cache

---
+++ deck-service/app/presentation/dependencies/deck.py
@@ -0,0 +1,15 @@
+from application.services.deck import DeckService
+from domain.repositories.cache import ICache
+from fastapi import Depends
+from infrastructure.repositories_impl.deck import DeckRepositoryImpl
+from presentation.dependencies.cache import get_cache
+from presentation.dependencies.db_session import get_db_session
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+def get_deck_service(
+    db_session: AsyncSession = Depends(get_db_session),
+    cache: ICache = Depends(get_cache),
+) -> DeckService:
+    deck_repository = DeckRepositoryImpl(db_session, cache)
+    return DeckService(deck_repository)
