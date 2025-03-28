925904b898febd875f23597417a28ed7adffca16
Nakakakilig <sparaska64@gmail.com>
2025-03-28 18:50:46 +0200

    refactor: replace core/schemas to application/schemas

--- swipe-service/app/application/schemas/swipe.py
+++ swipe-service/app/application/schemas/swipe.py
@@ -3,18 +3,18 @@ from datetime import datetime
 from pydantic import BaseModel
 
 
-class SwipeBase(BaseModel):
+class SwipeBaseSchema(BaseModel):
     profile_id_1: int
     profile_id_2: int
     decision_1: bool | None = None
     decision_2: bool | None = None
 
 
-class SwipeCreate(SwipeBase):
+class SwipeCreateSchema(SwipeBaseSchema):
     pass
 
 
-class SwipeRead(SwipeBase):
+class SwipeReadSchema(SwipeBaseSchema):
     id: int
     created_at: datetime
     updated_at: datetime
