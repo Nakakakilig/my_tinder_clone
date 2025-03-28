d50a97a865cb1e615cf99e4e111d7ce384c3f9e3
Nakakakilig <sparaska64@gmail.com>
2025-03-28 18:49:12 +0200

    Update script to change directory structure for running fake data generation

--- scripts/generate_fake_swipes.sh
+++ scripts/generate_fake_swipes.sh
@@ -9,8 +9,8 @@ echo -e "\nActivate venv ..."
 source .venv/bin/activate
 
 echo -e "\nRun swipe-service/app/utils/fake_data/main.py ..."
-cd swipe-service
-python3 -m app.utils.fake_data.main
+cd swipe-service/app
+python3 -m utils.fake_data.main
 
 echo -e "\nDeactivate venv ..."
 deactivate
