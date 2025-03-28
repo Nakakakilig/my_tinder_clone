765b562c5d0414a22cfa8a9f3e7a75f12a758f85
Nakakakilig <sparaska64@gmail.com>
2025-03-28 18:55:03 +0200

    feat: add get_swipe_service dependency for SwipeService instantiation

---
+++ swipe-service/app/presentation/dependencies/swipe.py
@@ -0,0 +1,12 @@
+from application.services.swipe import SwipeService
+from fastapi import Depends
+from infrastructure.repositories_impl.swipe import SwipeRepositoryImpl
+from presentation.dependencies.db_session import get_db_session
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+def get_swipe_service(
+    db_session: AsyncSession = Depends(get_db_session),
+) -> SwipeService:
+    swipe_repository = SwipeRepositoryImpl(db_session)
+    return SwipeService(swipe_repository)
