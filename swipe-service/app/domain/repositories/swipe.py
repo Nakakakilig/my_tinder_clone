9eb1b8a0b065e796414049477bf5312790c8fe9a
Nakakakilig <sparaska64@gmail.com>
2025-04-03 19:59:03 +0300

    refactor: enhance ISwipeRepository with type hints for methods

--- swipe-service/app/domain/repositories/swipe.py
+++ swipe-service/app/domain/repositories/swipe.py
@@ -1,19 +1,25 @@
 from abc import ABC, abstractmethod
 
+from domain.models.swipe import Swipe
+
 
 class ISwipeRepository(ABC):
     @abstractmethod
-    async def create_swipe(self):
+    async def create_swipe(self, swipe: Swipe) -> Swipe:
         raise NotImplementedError
 
     @abstractmethod
-    async def get_swipes(self):
+    async def get_swipes(self, limit: int, offset: int) -> list[Swipe] | None:
         raise NotImplementedError
 
     @abstractmethod
-    async def get_swipes_by_profile_id(self):
+    async def get_swipes_by_profile_id(
+        self, profile_id: int, limit: int, offset: int
+    ) -> list[Swipe] | None:
         raise NotImplementedError
 
     @abstractmethod
-    async def get_swipe_by_two_profile_ids(self):
+    async def get_swipe_by_two_profile_ids(
+        self, profile_id_1: int, profile_id_2: int
+    ) -> Swipe | None:
         raise NotImplementedError
