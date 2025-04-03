bfca46291c70adfe80cdd5348cbcd7e4f697273d
Nakakakilig <sparaska64@gmail.com>
2025-04-03 20:02:04 +0300

    refactor: update SwipeRepositoryImpl to use domain models and improve error handling

--- swipe-service/app/infrastructure/repositories_impl/swipe.py
+++ swipe-service/app/infrastructure/repositories_impl/swipe.py
@@ -1,9 +1,11 @@
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from application.schemas.swipe import SwipeCreateSchema
+from domain.exceptions import SwipeCreateError
+from domain.models.swipe import Swipe
 from domain.repositories.swipe import ISwipeRepository
 from infrastructure.db.db_models import SwipeORM
+from infrastructure.mappers.swipe import domain_to_orm, orm_to_domain
 
 
 class SwipeRepositoryImpl(ISwipeRepository):
@@ -13,37 +15,65 @@ class SwipeRepositoryImpl(ISwipeRepository):
     ):
         self.db_session = db_session
 
-    async def create_swipe(self, swipe: SwipeCreateSchema) -> SwipeORM:
-        swipe_orm = SwipeORM(**swipe.model_dump())
+    async def create_swipe(self, swipe: Swipe) -> Swipe:
+        swipe_orm = domain_to_orm(swipe)
         self.db_session.add(swipe_orm)
         await self.db_session.commit()
         await self.db_session.refresh(swipe_orm)
-        return swipe_orm
+        return orm_to_domain(swipe_orm)
 
-    async def get_swipes(self, limit: int, offset: int) -> list[SwipeORM] | None:
-        stmt = select(SwipeORM).order_by(SwipeORM.id.desc()).limit(limit).offset(offset)
-        result = await self.db_session.execute(stmt)
-        return result.scalars().all()
+    async def get_swipes(self, limit: int, offset: int) -> list[Swipe] | None:
+        try:
+            stmt = (
+                select(SwipeORM)
+                .order_by(SwipeORM.id.desc())
+                .limit(limit)
+                .offset(offset)
+            )
+            result = await self.db_session.execute(stmt)
+            swipes_orm = result.scalars().all()
+            if not swipes_orm:
+                return None
+            return [orm_to_domain(swipe_orm) for swipe_orm in swipes_orm]
+        except Exception as e:
+            raise SwipeCreateError(
+                "An error occurred while retrieving swipes"
+            ) from e
 
     async def get_swipes_by_profile_id(
         self, profile_id: int, limit: int, offset: int
-    ) -> list[SwipeORM] | None:
+    ) -> list[Swipe] | None:
         stmt = (
             select(SwipeORM)
-            .where((SwipeORM.profile_id_1 == profile_id) | (SwipeORM.profile_id_2 == profile_id))
+            .where(
+                (SwipeORM.profile_id_1 == profile_id)
+                | (SwipeORM.profile_id_2 == profile_id)
+            )
             .order_by(SwipeORM.id.desc())
             .limit(limit)
             .offset(offset)
         )
         result = await self.db_session.execute(stmt)
-        return result.scalars().all()
+        swipes_orm = result.scalars().all()
+        if not swipes_orm:
+            return None
+        return [orm_to_domain(swipe_orm) for swipe_orm in swipes_orm]
 
     async def get_swipe_by_two_profile_ids(
         self, profile_id_1: int, profile_id_2: int
-    ) -> SwipeORM | None:
+    ) -> Swipe | None:
         stmt = select(SwipeORM).where(
-            ((SwipeORM.profile_id_1 == profile_id_1) & (SwipeORM.profile_id_2 == profile_id_2))
-            | ((SwipeORM.profile_id_1 == profile_id_2) & (SwipeORM.profile_id_2 == profile_id_1))
+            (
+                (SwipeORM.profile_id_1 == profile_id_1)
+                & (SwipeORM.profile_id_2 == profile_id_2)
+            )
+            | (
+                (SwipeORM.profile_id_1 == profile_id_2)
+                & (SwipeORM.profile_id_2 == profile_id_1)
+            )
         )
         result = await self.db_session.execute(stmt)
-        return result.scalar_one_or_none()
+        swipe_orm = result.scalar_one_or_none()
+        if not swipe_orm:
+            return None
+        return orm_to_domain(swipe_orm)
