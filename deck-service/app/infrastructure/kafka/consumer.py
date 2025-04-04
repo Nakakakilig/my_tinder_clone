f6f1f8068e8137771884cfdafa2ca13d3c15ac03
Nakakakilig <sparaska64@gmail.com>
2025-04-04 03:42:28 +0300

    refactor: add type ignore comments for better type safety in Kafka consumer

--- deck-service/app/infrastructure/kafka/consumer.py
+++ deck-service/app/infrastructure/kafka/consumer.py
@@ -2,8 +2,8 @@ import asyncio
 import json
 from time import sleep
 
-from aiokafka import AIOKafkaConsumer
-from aiokafka.errors import KafkaConnectionError
+from aiokafka import AIOKafkaConsumer  # type: ignore
+from aiokafka.errors import KafkaConnectionError  # type: ignore
 
 from config.settings import settings
 from infrastructure.kafka.events import handle_event
@@ -25,7 +25,7 @@ async def consume():
         topic,
         bootstrap_servers=bootstrap_servers,
         group_id=group_id,
-        value_deserializer=lambda m: json.loads(m.decode("utf-8")),
+        value_deserializer=lambda m: json.loads(m.decode("utf-8")),  # type: ignore
     )
     consume_started = False
     while not consume_started:
@@ -35,17 +35,18 @@ async def consume():
         except KafkaConnectionError:
             consume_started = False
             print("CONSUMER CANT START. SLEEP FOR 5 SECONDS")
+            # yes, its a bad practice, but i need here for easy debugging
             sleep(5)
 
     try:
         print("Kafka consumer started and listening...")
-        async for message in consumer:
-            event = message.value
-            await handle_event(event)
+        async for message in consumer:  # type: ignore
+            event = message.value  # type: ignore
+            await handle_event(event)  # type: ignore
     finally:
         await consumer.stop()
 
 
 async def start_consumer_loop():
     loop = asyncio.get_event_loop()
-    loop.create_task(consume())
+    _ = await loop.create_task(consume())
