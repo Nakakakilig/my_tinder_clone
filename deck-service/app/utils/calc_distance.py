de91311b6419e62b01cb1b95d638a1e9c0542352
Nakakakilig <sparaska64@gmail.com>
2025-04-04 04:13:01 +0300

    refactor: update distance calculation to use ProfileORM and improve parameter naming

--- deck-service/app/utils/calc_distance.py
+++ deck-service/app/utils/calc_distance.py
@@ -3,23 +3,23 @@ from typing import TYPE_CHECKING
 from sqlalchemy import Numeric, cast, func
 
 if TYPE_CHECKING:
-    from domain.models.profile import Profile
+    from infrastructure.db.db_models import ProfileORM
 
 
-def calc_distance_in_query(current_profile: "Profile", Profile: "Profile"):
+def calc_distance_in_query(current_profile: "ProfileORM", profile_orm: "ProfileORM"):
     # TODO: add PostGis instead of raw SQL
     distance_expr = func.round(
         cast(
             6371  # radius of the earth in km
             * func.acos(
                 func.cos(func.radians(current_profile.geo_latitude))
-                * func.cos(func.radians(Profile.geo_latitude))
+                * func.cos(func.radians(profile_orm.geo_latitude))
                 * func.cos(
-                    func.radians(Profile.geo_longitude)
-                    - func.radians(current_profile.geo_longitude)
+                    func.radians(profile_orm.geo_longitude)
+                    - func.radians(current_profile.geo_longitude),
                 )
                 + func.sin(func.radians(current_profile.geo_latitude))
-                * func.sin(func.radians(Profile.geo_latitude))
+                * func.sin(func.radians(profile_orm.geo_latitude)),
             ),
             Numeric,
         ),
