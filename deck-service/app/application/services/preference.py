e922c02e8f6ba2e94ab58b0b77fdc9989dae60e3
Nakakakilig <sparaska64@gmail.com>
2025-04-07 23:52:57 +0300

    refactor: update return types in PreferenceService methods to allow for None values

--- deck-service/app/application/services/preference.py
+++ deck-service/app/application/services/preference.py
@@ -9,13 +9,13 @@ class PreferenceService:
     ):
         self.preference_repository = preference_repository
 
-    async def get_preference_by_id(self, preference_id: int) -> Preference:
+    async def get_preference_by_id(self, preference_id: int) -> Preference | None:
         return await self.preference_repository.get_preference_by_id(preference_id)
 
-    async def get_preference_by_profile_id(self, profile_id: int) -> Preference:
+    async def get_preference_by_profile_id(self, profile_id: int) -> Preference | None:
         return await self.preference_repository.get_preference_by_profile_id(profile_id)
 
-    async def get_preferences(self) -> list[Preference]:
+    async def get_preferences(self) -> list[Preference] | None:
         return await self.preference_repository.get_preferences()
 
     async def create_preference(self, preference: Preference) -> Preference | None:
