eb4d229b19f7e12ab06a4f93d6b421478bf90337
Nakakakilig <sparaska64@gmail.com>
2025-04-07 22:59:46 +0300

    refactor: enhance exception handling in swipe service with detailed error messages

--- swipe-service/app/domain/exceptions.py
+++ swipe-service/app/domain/exceptions.py
@@ -1,10 +1,23 @@
 class SwipeAlreadyExistsError(Exception):
-    pass
-
-
-class SwipeNotFoundError(Exception):
-    pass
+    def __init__(self, profile_id_1: int, profile_id_2: int):
+        super().__init__(f"Swipe already exists for profiles {profile_id_1} and {profile_id_2}")
 
 
 class SwipeCreateError(Exception):
-    pass
+    def __init__(self, profile_id_1: int, profile_id_2: int):
+        super().__init__(f"Error creating swipe for profiles {profile_id_1} and {profile_id_2}")
+
+
+class SwipeNotFoundError(Exception):
+    def __init__(self, profile_id_1: int | None = None, profile_id_2: int | None = None):
+        if profile_id_1 and not profile_id_2:
+            super().__init__(f"Swipes for {profile_id_1} not found")
+        elif profile_id_1 and profile_id_2:
+            if profile_id_1 == profile_id_2:
+                super().__init__(
+                    "Profile_1 ID cannot be the same as Profile_2 ID. Please check the input data."
+                )
+            else:
+                super().__init__(f"Swipes for profiles {profile_id_1} and {profile_id_2} not found")
+        else:
+            super().__init__("Swipes not found")
