0928fbc6d25e72eb0cb2dcd1c3c4cea62a49e7ac
Nakakakilig <sparaska64@gmail.com>
2025-04-07 23:21:08 +0300

    refactor: move logic of generate_deck from repo_impl to deck_service

--- deck-service/app/application/services/deck.py
+++ deck-service/app/application/services/deck.py
@@ -1,19 +1,66 @@
-from domain.models.deck import MatchDeck
+from application.schemas.profile import ProfileWithDistance
+from domain.exceptions import (
+    CandidateNotFoundError,
+    DeckCacheError,
+    DeckGenerateError,
+    ProfileNotFoundError,
+)
+from domain.models.deck import MatchCard, MatchDeck
 from domain.repositories.deck import IDeckRepository
+from domain.repositories.profile import IProfileRepository
+from domain.repositories.swipe import ISwipeRepository
 
 
 class DeckService:
     def __init__(
         self,
         deck_repository: IDeckRepository,
+        profile_repository: IProfileRepository,
+        swipe_client: ISwipeRepository,
     ):
         self.deck_repository = deck_repository
+        self.profile_repository = profile_repository
+        self.swipe_client = swipe_client
 
     async def get_deck_by_id(self, profile_id: int) -> MatchDeck:
-        return await self.deck_repository.get_deck_by_id(profile_id)
+        deck = await self.deck_repository.get_deck_by_id(profile_id)
+        if not deck:
+            raise DeckCacheError(profile_id)
+        return deck
 
     async def generate_deck_by_id(self, profile_id: int, limit: int) -> MatchDeck:
-        return await self.deck_repository.generate_deck_by_id(profile_id, limit)
+        try:
+            if not profile_id:
+                raise ProfileNotFoundError(profile_id)
+
+            candidates_and_distance = await self.profile_repository.get_candidates_and_distance(
+                profile_id, limit
+            )
+            if not candidates_and_distance:
+                raise CandidateNotFoundError(profile_id)
+
+            profiles_with_distance = [
+                ProfileWithDistance(**candidate.__dict__, distance_km=distance)
+                for candidate, distance in candidates_and_distance
+            ]
+
+            candidates = await self._filter_profiles_by_swipe(profile_id, profiles_with_distance)
+
+            deck = MatchDeck(
+                profile_id=profile_id,
+                candidates=[
+                    MatchCard(profile_id=candidate.id, **candidate.__dict__)
+                    for candidate in candidates
+                ],
+            )
+
+            await self.deck_repository.save_deck(deck)
+            return deck  # noqa: TRY300
+
+        except DeckCacheError as e:
+            raise DeckCacheError(profile_id) from e
+        except Exception as e:
+            raise DeckGenerateError(profile_id, str(e)) from e
 
     async def get_all_decks(self) -> list[MatchDeck]:
         return await self.deck_repository.get_all_decks()
@@ -23,3 +70,21 @@ class DeckService:
 
     async def clear_all_deck_cache(self) -> None:
         return await self.deck_repository.clear_all_deck_cache()
+
+    async def _filter_profiles_by_swipe(
+        self,
+        profile_id: int,
+        profiles_with_distance: list[ProfileWithDistance],
+    ) -> list[ProfileWithDistance]:
+        candidates: list[ProfileWithDistance] = []
+        for candidate in profiles_with_distance:
+            swipe = await self.swipe_client.get_swipe_by_two_profile_ids(candidate.id, profile_id)
+            if not swipe:
+                # profile_id has not swiped on candidate
+                candidates.append(candidate)
+            elif (profile_id == swipe.profile_id_1 and not swipe.decision_1) or (
+                profile_id == swipe.profile_id_2 and not swipe.decision_2
+            ):
+                # profile_id has not liked candidate
+                candidates.append(candidate)
+        return candidates
--- deck-service/app/infrastructure/repositories_impl/deck.py
+++ deck-service/app/infrastructure/repositories_impl/deck.py
@@ -1,20 +1,9 @@
-from sqlalchemy import and_, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from config.settings import settings
-from domain.exceptions import (
-    CandidateNotFoundError,
-    DeckCacheError,
-    DeckGenerateError,
-    PreferenceForProfileNotFoundError,
-    PreferenceNotFoundError,
-    ProfileNotFoundError,
-)
-from domain.models.deck import MatchCard, MatchDeck
+from domain.exceptions import DeckCacheError
+from domain.models.deck import MatchDeck
 from domain.repositories.cache import ICache
 from domain.repositories.deck import IDeckRepository
-from infrastructure.db.db_models import ProfileORM
-from utils.calc_distance import calc_distance_in_query
 
 
 class DeckRepositoryImpl(IDeckRepository):
@@ -28,30 +17,13 @@ class DeckRepositoryImpl(IDeckRepository):
             raise DeckCacheError(profile_id)
         return MatchDeck(**deck_data)
 
-    async def generate_deck_by_id(self, profile_id: int, limit: int):
-        try:
-            profiles_and_distance = await self._get_matching_profiles_and_distance(
-                profile_id, limit
-            )
-            cards: list[MatchCard] = await self._convert_to_cards(profiles_and_distance)
-            deck = MatchDeck(profile_id=profile_id, candidates=cards)
-            await self.cache.set(f"deck:{profile_id}", deck.model_dump())
-            return deck  # noqa: TRY300
-
-        except CandidateNotFoundError as e:
-            raise CandidateNotFoundError(profile_id) from e
-        except ProfileNotFoundError as e:
-            raise ProfileNotFoundError(profile_id) from e
-        except PreferenceNotFoundError as e:
-            raise PreferenceNotFoundError(profile_id) from e
-        except DeckCacheError as e:
-            raise DeckCacheError(profile_id) from e
-        except Exception as e:
-            raise DeckGenerateError(profile_id) from e
+    async def save_deck(self, deck: MatchDeck) -> None:
+        await self.cache.set(f"deck:{deck.profile_id}", deck.model_dump())
+        return
 
     async def clear_deck_cache_by_id(self, profile_id: int) -> None:
         await self.cache.delete(f"deck:{profile_id}")
-        return  # noqa: PLR1711
+        return
 
     async def get_all_decks(self) -> list[MatchDeck]:
         decks_data = await self.cache.get_all_values()
@@ -59,52 +31,8 @@ class DeckRepositoryImpl(IDeckRepository):
             raise DeckCacheError()
         decks = [MatchDeck(**deck_data) for deck_data in decks_data]
         sorted_decks = sorted(decks, key=lambda x: x.profile_id)
-        return sorted_decks  # noqa: RET504
+        return sorted_decks
 
     async def clear_all_deck_cache(self) -> None:
         await self.cache.clear()
-        return  # noqa: PLR1711
-
-    async def _get_matching_profiles_and_distance(
-        self, profile_id: int, limit: int
-    ) -> list[tuple[ProfileORM, float]]:
-        result = await self.db_session.execute(
-            select(ProfileORM).where(ProfileORM.id == profile_id)
-        )
-        profile = result.scalars().first()
-
-        if not profile:
-            raise ProfileNotFoundError(profile_id)
-
-        preference = profile.preference
-        if not preference:
-            raise PreferenceForProfileNotFoundError(profile_id)
-        distance_expr = calc_distance_in_query(profile, ProfileORM)  # type: ignore
-
-        filters = [
-            ProfileORM.gender == preference.gender,
-            ProfileORM.age >= preference.age - settings.deck.age_range,
-            ProfileORM.age <= preference.age + settings.deck.age_range,
-        ]
-        query = (
-            select(ProfileORM, distance_expr)
-            .filter(and_(*filters))
-            .group_by(ProfileORM.id)
-            .order_by(distance_expr)
-            .limit(limit)
-        )
-
-        query = query.having(distance_expr <= preference.radius)
-
-        result = await self.db_session.execute(query)
-        profiles_and_distance: list[tuple[ProfileORM, float]] = result.all()  # type: ignore
-        return profiles_and_distance
-
-    async def _convert_to_cards(
-        self, profile_and_distance: list[tuple[ProfileORM, float]]
-    ) -> list[MatchCard]:
-        cards: list[MatchCard] = []
-        for profile, distance in profile_and_distance:
-            card = MatchCard(distance_km=distance, profile_id=profile.id, **profile.__dict__)
-            cards.append(card)
-        return cards
+        return
