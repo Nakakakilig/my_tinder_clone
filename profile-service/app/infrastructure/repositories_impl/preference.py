f859128b5120f404c1f7df15258127736f8772cf
Nakakakilig <sparaska64@gmail.com>
2025-04-08 15:41:36 +0300

    refactor: update preference repository methods to return domain models

--- profile-service/app/infrastructure/repositories_impl/preference.py
+++ profile-service/app/infrastructure/repositories_impl/preference.py
@@ -1,7 +1,10 @@
-from application.schemas.preference import PreferenceCreateSchema
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from domain.models.preference import Preference
 from domain.repositories.preference import IPreferenceRepository
 from infrastructure.db.db_models import PreferenceORM
-from domain.models.preference import Preference
+from infrastructure.mappers.preference import orm_to_domain
 
 
 class PreferenceRepositoryImpl(IPreferenceRepository):
@@ -11,21 +14,28 @@ class PreferenceRepositoryImpl(IPreferenceRepository):
     async def get_preference_by_id(self, preference_id: int) -> Preference | None:
         stmt = select(PreferenceORM).where(PreferenceORM.id == preference_id)
         result = await self.db_session.execute(stmt)
-        return result.scalar_one_or_none()
+        preference_orm = result.scalar_one_or_none()
+        if preference_orm is None:
+            return None
+        return orm_to_domain(preference_orm)
 
     async def get_preference_by_profile_id(self, profile_id: int) -> Preference | None:
         stmt = select(PreferenceORM).where(PreferenceORM.profile_id == profile_id)
         result = await self.db_session.execute(stmt)
-        return result.scalar_one_or_none()
+        preference_orm = result.scalar_one_or_none()
+        if preference_orm is None:
+            return None
+        return orm_to_domain(preference_orm)
 
     async def create_preference(self, preference: Preference) -> Preference:
         preference_orm = PreferenceORM(**preference.model_dump())
         self.db_session.add(preference_orm)
         await self.db_session.commit()
         await self.db_session.refresh(preference_orm)
-        return preference_orm
+        return orm_to_domain(preference_orm)
 
     async def get_preferences(self) -> list[Preference]:
         stmt = select(PreferenceORM).order_by(PreferenceORM.id)
         result = await self.db_session.execute(stmt)
-        return result.scalars().all()
+        preference_orms = result.scalars().all()
+        return [orm_to_domain(preference_orm) for preference_orm in preference_orms]
