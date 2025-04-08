79021534a4fbcba8a50a52bd824628ab3645d4d8
Nakakakilig <sparaska64@gmail.com>
2025-04-08 17:52:33 +0300

    feat: move UserService to user_cases/ and update imports in user routes

--- profile-service/app/presentation/dependencies/user.py
+++ profile-service/app/presentation/dependencies/user.py
@@ -1,8 +1,9 @@
-from application.services.user import UserService
 from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from infrastructure.repositories_impl.user import UserRepositoryImpl
 from presentation.dependencies.db_session import get_db_session
-from sqlalchemy.ext.asyncio import AsyncSession
+from use_cases.user import UserService
 
 
 def get_user_service(
