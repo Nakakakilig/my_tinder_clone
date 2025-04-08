a89abd76402da7f9d49f9e30f3db8e62a2538860
Nakakakilig <sparaska64@gmail.com>
2025-04-08 15:28:22 +0300

    refactor: enhance type annotations in get_preference_service

--- profile-service/app/presentation/dependencies/preference.py
+++ profile-service/app/presentation/dependencies/preference.py
@@ -5,11 +5,12 @@ from infrastructure.kafka.producer import KafkaProducer
 from infrastructure.repositories_impl.preference import PreferenceRepositoryImpl
 from presentation.dependencies.db_session import get_db_session
 from sqlalchemy.ext.asyncio import AsyncSession
+from typing import Annotated
 
 
 def get_preference_service(
-    db_session: AsyncSession = Depends(get_db_session),
-    kafka_producer: KafkaProducer = Depends(get_kafka_producer),
+    db_session: Annotated[AsyncSession, Depends(get_db_session)],
+    kafka_producer: Annotated[KafkaProducer, Depends(get_kafka_producer)],
 ) -> PreferenceService:
     preference_repository = PreferenceRepositoryImpl(db_session)
     return PreferenceService(preference_repository, kafka_producer)
