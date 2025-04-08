44edd878161fe8715835c61e901c2cd9c743df56
Nakakakilig <sparaska64@gmail.com>
2025-04-08 14:59:29 +0300

    refactor: enhance profile route handlers with type annotations and handle None responses

--- profile-service/app/presentation/routes/profiles.py
+++ profile-service/app/presentation/routes/profiles.py
@@ -6,33 +6,30 @@ from presentation.dependencies.profile import get_profile_service
 router = APIRouter(tags=["profiles"])
 
 
-@router.get("/", response_model=list[ProfileReadSchema])
+@router.get("/")
 async def get_profiles(
-    profile_service: ProfileService = Depends(get_profile_service),
-):
-    return await profile_service.get_profiles()
+    profile_service: Annotated[ProfileService, Depends(get_profile_service)],
+) -> list[ProfileReadSchema] | None:
 
 
-@router.post("/", response_model=ProfileReadSchema)
+@router.post("/")
 async def create_profile(
     profile_create: ProfileCreateSchema,
-    profile_service: ProfileService = Depends(get_profile_service),
+    profile_service: Annotated[ProfileService, Depends(get_profile_service)],
     # TODO in future:  user_id: UUID = Depends(get_user_id_from_JWT_token)
 ) -> ProfileReadSchema:
     return await profile_service.create_profile(profile_create)
 
 
-@router.get("/{profile_id}", response_model=ProfileReadSchema)
+@router.get("/{profile_id}")
 async def get_profile(
     profile_id: int,
-    profile_service: ProfileService = Depends(get_profile_service),
-) -> ProfileReadSchema:
-    return await profile_service.get_profile_by_id(profile_id)
+    profile_service: Annotated[ProfileService, Depends(get_profile_service)],
+) -> ProfileReadSchema | None:
 
 
-@router.get("/user/{user_id}", response_model=ProfileReadSchema)
+@router.get("/user/{user_id}")
 async def get_profile_by_user_id(
     user_id: int,
-    profile_service: ProfileService = Depends(get_profile_service),
-) -> ProfileReadSchema:
-    return await profile_service.get_profile_by_user_id(user_id)
+    profile_service: Annotated[ProfileService, Depends(get_profile_service)],
+) -> ProfileReadSchema | None:
