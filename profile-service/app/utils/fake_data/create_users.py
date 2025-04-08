0b993f8925f186a679c04392a769935fa5181ff9
Nakakakilig <sparaska64@gmail.com>
2025-04-08 18:02:23 +0300

    feat: enhance user creation process by adding error handling for existing users and updating function parameters for clarity

--- profile-service/app/utils/fake_data/create_users.py
+++ profile-service/app/utils/fake_data/create_users.py
@@ -1,22 +1,26 @@
 import asyncio
+import contextlib
 
-from application.schemas.user import UserCreateSchema
 from faker import Faker
+
+from domain.exceptions import UserAlreadyExistsError
+from domain.models.user import User
 from infrastructure.db.db_helper import db_helper
 from presentation.dependencies.user import get_user_service
+from presentation.schemas.user import UserCreateSchema
 
 fake = Faker("uk_UA")
 
 
-async def create_multiple_users(N_users: int = 100):
-    user_creates = [
-        UserCreateSchema(username=fake.user_name()) for _ in range(1, N_users + 1)
-    ]
+async def create_multiple_users(n_users: int = 100):
+    user_creates = [UserCreateSchema(username=fake.user_name()) for _ in range(1, n_users + 1)]
 
     await asyncio.sleep(5)
 
     async for session in db_helper.session_getter():
         for user_create in user_creates:
-            await get_user_service(session).create_user(user_create)
-
+            user = User(**user_create.model_dump())
+            user_service = get_user_service(session)
+            with contextlib.suppress(UserAlreadyExistsError):  # skip error
+                await user_service.create_user(user)
     print(f"Successfully created {len(user_creates)} users")
