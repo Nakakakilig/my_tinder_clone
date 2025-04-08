9c5618593b61ec4c08cd4840e775349113096cb8
Nakakakilig <sparaska64@gmail.com>
2025-04-08 14:52:42 +0300

    refactor: streamline UserORM and ProfileORM model definitions by consolidating mapped_column declarations

--- profile-service/app/infrastructure/db/db_models.py
+++ profile-service/app/infrastructure/db/db_models.py
@@ -1,16 +1,15 @@
-from domain.enums import Gender
-from infrastructure.db.base import Base
 from sqlalchemy import DateTime, Enum, Float, ForeignKey, Integer, String, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from domain.enums import Gender
+from infrastructure.db.base import Base
 
 
 class UserORM(Base):
     __tablename__ = "users"
 
     username: Mapped[str] = mapped_column(unique=True)
-    created_at: Mapped[DateTime] = mapped_column(
-        DateTime(timezone=True), server_default=func.now()
-    )
+    created_at: Mapped[DateTime] = mapped_column(DateTime(timezone=True), server_default=func.now())
     updated_at: Mapped[DateTime] = mapped_column(
         DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
     )
@@ -21,18 +20,14 @@ class UserORM(Base):
 class ProfileORM(Base):
     __tablename__ = "profiles"
 
-    user_id: Mapped[int] = mapped_column(
-        Integer, ForeignKey("users.id"), index=True, unique=True
-    )
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"), index=True, unique=True)
     first_name: Mapped[str] = mapped_column(String(20), nullable=False)
     last_name: Mapped[str] = mapped_column(String(20), nullable=False)
     gender: Mapped[Gender] = mapped_column(Enum(Gender), nullable=False)
     age: Mapped[int] = mapped_column(Integer, nullable=False)
     geo_latitude: Mapped[float] = mapped_column(Float, nullable=False)
     geo_longitude: Mapped[float] = mapped_column(Float, nullable=False)
-    created_at: Mapped[DateTime] = mapped_column(
-        DateTime(timezone=True), server_default=func.now()
-    )
+    created_at: Mapped[DateTime] = mapped_column(DateTime(timezone=True), server_default=func.now())
     updated_at: Mapped[DateTime] = mapped_column(
         DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
     )
