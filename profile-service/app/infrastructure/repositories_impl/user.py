65c5382e4e7a22a6b579f2d97c50d0128550c00b
Nakakakilig <sparaska64@gmail.com>
2025-04-08 17:48:32 +0300

    feat: implement user ORM mapping functions and update UserRepositoryImpl methods for improved user handling

--- profile-service/app/infrastructure/repositories_impl/user.py
+++ profile-service/app/infrastructure/repositories_impl/user.py
@@ -1,9 +1,11 @@
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from domain.exceptions import UserAlreadyExistsError
 from domain.models.user import User
 from domain.repositories.user import IUserRepository
 from infrastructure.db.db_models import UserORM
+from infrastructure.mappers.user import domain_to_orm, orm_to_domain
 
 
 class UserRepositoryImpl(IUserRepository):
@@ -14,6 +16,10 @@ class UserRepositoryImpl(IUserRepository):
         stmt = select(UserORM).where(UserORM.id == user_id)
         result = await self.db_session.execute(stmt)
         user_orm = result.scalar_one_or_none()
+        if user_orm is None:
+            return None
+        return orm_to_domain(user_orm)
+
     async def get_user_by_username(self, username: str) -> User | None:
         stmt = select(UserORM).where(UserORM.username == username)
         result = await self.db_session.execute(stmt)
@@ -23,14 +29,16 @@ class UserRepositoryImpl(IUserRepository):
         return orm_to_domain(user_orm)
 
     async def create_user(self, user: User) -> User:
-        user_orm = UserORM(**user.model_dump())
+        if await self.get_user_by_username(user.username):
+            raise UserAlreadyExistsError(user.username)
+        user_orm = domain_to_orm(user)
         self.db_session.add(user_orm)
         await self.db_session.commit()
         await self.db_session.refresh(user_orm)
-        return user_orm
+        return orm_to_domain(user_orm)
 
     async def get_users(self) -> list[User]:
         stmt = select(UserORM).order_by(UserORM.id)
         result = await self.db_session.execute(stmt)
         users_orm = result.scalars().all()
-        return users_orm
+        return [orm_to_domain(user_orm) for user_orm in users_orm]
