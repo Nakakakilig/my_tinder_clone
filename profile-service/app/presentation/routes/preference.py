43bd341abc83768a0282eca26b4d6dc04bc9f976
Nakakakilig <sparaska64@gmail.com>
2025-04-08 15:42:13 +0300

    feat: implement mappers for preference domain and presentation layers

--- profile-service/app/presentation/routes/preference.py
+++ profile-service/app/presentation/routes/preference.py
@@ -4,7 +4,12 @@ from fastapi import APIRouter, Depends
 
 from application.schemas.preference import PreferenceCreateSchema, PreferenceReadSchema
 from application.services.preference import PreferenceService
+from domain.models.preference import Preference
 from presentation.dependencies.preference import get_preference_service
+from presentation.mappers.preference import (
+    preference_to_read_schema,
+    preferences_to_read_schema_list,
+)
 
 router = APIRouter(tags=["preferences"])
 
@@ -13,7 +18,10 @@ router = APIRouter(tags=["preferences"])
 async def get_preferences(
     preference_service: Annotated[PreferenceService, Depends(get_preference_service)],
 ) -> list[PreferenceReadSchema] | None:
-    return await preference_service.get_preferences()
+    preferences = await preference_service.get_preferences()
+    if not preferences:
+        return None
+    return preferences_to_read_schema_list(preferences)
 
 
 @router.post("/")
@@ -21,8 +29,10 @@ async def create_preference(
     preference_create: PreferenceCreateSchema,
     preference_service: Annotated[PreferenceService, Depends(get_preference_service)],
     # TODO in future:  user_id: UUID = Depends(get_user_id_from_JWT_token)
-) -> PreferenceReadSchema | None:
-    return await preference_service.create_preference(preference_create)
+) -> PreferenceReadSchema:
+    preference_model = Preference(**preference_create.model_dump())
+    preference = await preference_service.create_preference(preference_model)
+    return preference_to_read_schema(preference)
 
 
 @router.get("/{preference_id}")
@@ -30,7 +40,10 @@ async def get_preference_by_id(
     preference_id: int,
     preference_service: Annotated[PreferenceService, Depends(get_preference_service)],
 ) -> PreferenceReadSchema | None:
-    return await preference_service.get_preference_by_id(preference_id)
+    preference = await preference_service.get_preference_by_id(preference_id)
+    if not preference:
+        return None
+    return preference_to_read_schema(preference)
 
 
 @router.get("/profile/{profile_id}")
@@ -38,7 +51,10 @@ async def get_preference_by_profile_id(
     profile_id: int,
     preference_service: Annotated[PreferenceService, Depends(get_preference_service)],
 ) -> PreferenceReadSchema | None:
-    return await preference_service.get_preference_by_profile_id(profile_id)
+    preference = await preference_service.get_preference_by_profile_id(profile_id)
+    if not preference:
+        return None
+    return preference_to_read_schema(preference)
 
 
 # TODO: when want to update preference - generate new deck
