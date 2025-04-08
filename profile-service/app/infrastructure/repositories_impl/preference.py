064aa84df7cb542d50faac3145035fbefb74f0fe
Nakakakilig <sparaska64@gmail.com>
2025-04-08 15:22:31 +0300

    refactor: update type annotations in preference repository

--- profile-service/app/infrastructure/repositories_impl/preference.py
+++ profile-service/app/infrastructure/repositories_impl/preference.py
@@ -1,36 +1,31 @@
 from application.schemas.preference import PreferenceCreateSchema
 from domain.repositories.preference import IPreferenceRepository
 from infrastructure.db.db_models import PreferenceORM
-from sqlalchemy import select
-from sqlalchemy.ext.asyncio import AsyncSession
+from domain.models.preference import Preference
 
 
 class PreferenceRepositoryImpl(IPreferenceRepository):
     def __init__(self, db_session: AsyncSession):
         self.db_session = db_session
 
-    async def get_preference_by_id(self, preference_id: int) -> PreferenceORM | None:
+    async def get_preference_by_id(self, preference_id: int) -> Preference | None:
         stmt = select(PreferenceORM).where(PreferenceORM.id == preference_id)
         result = await self.db_session.execute(stmt)
         return result.scalar_one_or_none()
 
-    async def get_preference_by_profile_id(
-        self, profile_id: int
-    ) -> PreferenceORM | None:
+    async def get_preference_by_profile_id(self, profile_id: int) -> Preference | None:
         stmt = select(PreferenceORM).where(PreferenceORM.profile_id == profile_id)
         result = await self.db_session.execute(stmt)
         return result.scalar_one_or_none()
 
-    async def create_preference(
-        self, preference: PreferenceCreateSchema
-    ) -> PreferenceORM:
+    async def create_preference(self, preference: Preference) -> Preference:
         preference_orm = PreferenceORM(**preference.model_dump())
         self.db_session.add(preference_orm)
         await self.db_session.commit()
         await self.db_session.refresh(preference_orm)
         return preference_orm
 
-    async def get_preferences(self) -> list[PreferenceORM]:
+    async def get_preferences(self) -> list[Preference]:
         stmt = select(PreferenceORM).order_by(PreferenceORM.id)
         result = await self.db_session.execute(stmt)
         return result.scalars().all()
