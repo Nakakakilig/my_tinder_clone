5b1feafd01f0e9c3e6e1fb85b861f72e77be4824
Nakakakilig <sparaska64@gmail.com>
2025-04-08 15:00:32 +0300

    refactor: update create_multiple_profiles function to use consistent variable naming and improve profile creation logic

--- profile-service/app/utils/fake_data/create_profiles.py
+++ profile-service/app/utils/fake_data/create_profiles.py
@@ -1,10 +1,12 @@
 import asyncio
 import random
 
+from faker import Faker
+
 from application.schemas.profile import ProfileCreateSchema
 from application.services.profile import ProfileService
 from domain.enums import Gender
-from faker import Faker
+from domain.models.profile import Profile
 from infrastructure.db.db_helper import db_helper
 from infrastructure.kafka.init import get_kafka_producer
 from infrastructure.repositories_impl.profile import ProfileRepositoryImpl
@@ -19,7 +21,7 @@ BOTTOM_LEFT_LONGITUDE = 40.12
 
 
 async def create_multiple_profiles(
-    N_profiles: int = 100,
+    n_profiles: int = 100,
 ):
     profile_creates = [
         ProfileCreateSchema(
@@ -31,7 +33,7 @@ async def create_multiple_profiles(
             geo_latitude=random.uniform(BOTTOM_LEFT_LATITUDE, TOP_RIGHT_LATITUDE),
             geo_longitude=random.uniform(BOTTOM_LEFT_LONGITUDE, TOP_RIGHT_LONGITUDE),
         )
-        for i in range(1, N_profiles + 1)
+        for i in range(1, n_profiles + 1)
     ]
 
     await asyncio.sleep(5)
@@ -39,10 +41,11 @@ async def create_multiple_profiles(
     kafka_producer = get_kafka_producer()
     for profile_create in profile_creates:
         async for session in db_helper.session_getter():
+            profile = Profile(**profile_create.model_dump())
             profile_service = ProfileService(
                 profile_repository=ProfileRepositoryImpl(session),
                 kafka_producer=kafka_producer,
             )
-            await profile_service.create_profile(profile_create)
-
-    print(f"Created {N_profiles} profiles")
+            # todo: add exception handling
+            await profile_service.create_profile(profile)
+    print(f"Created {n_profiles} profiles")
